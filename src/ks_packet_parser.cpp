b5ae40b43563d3e90dfb0d909197c3534a165757
Cristian Milatinov <milatinovcristian@gmail.com>
2023-01-23 00:12:55 -0500

    Change pointers to references

--- src/ks_packet_parser.cpp
+++ src/ks_packet_parser.cpp
@@ -4,8 +4,8 @@ namespace kronos {
 
     static uint32_t s_NextPacketId = 0;
 
-    void EncodePacket(Packet* dest, uint8_t flags, uint16_t command, const uint8_t* payload, uint8_t payloadSize) {
-        *dest = {
+    void EncodePacket(Packet& dest, uint8_t flags, uint16_t command, const uint8_t* payload, uint8_t payloadSize) {
+        dest = {
             .Header = {
                 .Magic = KSP_MAGIC,
                 .PacketId = s_NextPacketId++,
@@ -16,22 +16,22 @@ namespace kronos {
             },
             .Payload = {}
         };
-        memcpy(dest->Payload, payload, payloadSize);
-        dest->Header.CheckSum = CRC32((uint8_t*) &dest, sizeof(dest->Header) + dest->Header.PayloadSize);
+        memcpy(dest.Payload, payload, payloadSize);
+        dest.Header.CheckSum = CRC32((uint8_t*) &dest, sizeof(dest.Header) + dest.Header.PayloadSize);
     }
 
-    int32_t ValidatePacketHeader(PacketHeader* header) {
-        if (header->Magic != KSP_MAGIC)
+    int32_t ValidatePacketHeader(const PacketHeader& header) {
+        if (header.Magic != KSP_MAGIC)
             return -1;
-        return (int32_t) header->PayloadSize;
+        return (int32_t) header.PayloadSize;
     }
 
-    bool ValidatePacket(Packet* packet) {
-        uint32_t crc = packet->Header.CheckSum;
-        packet->Header.CheckSum = 0;
+    bool ValidatePacket(Packet& packet) {
+        uint32_t crc = packet.Header.CheckSum;
+        packet.Header.CheckSum = 0;
 
-        uint32_t packetCrc = CRC32((uint8_t*) packet, sizeof(packet->Header) + packet->Header.PayloadSize);
-        packet->Header.CheckSum = crc;
+        uint32_t packetCrc = CRC32((uint8_t*) &packet, sizeof(packet.Header) + packet.Header.PayloadSize);
+        packet.Header.CheckSum = crc;
         if (packetCrc != crc)
             return false;
 
--- src/ks_packet_parser.h
+++ src/ks_packet_parser.h
@@ -12,9 +12,9 @@ void* memcpy(void* dest, const void* src, size_t n);
 
 namespace kronos {
 
-    void EncodePacket(Packet* dest, uint8_t flags, uint16_t command, const uint8_t* payload, uint8_t payloadSize);
+    void EncodePacket(Packet& dest, uint8_t flags, uint16_t command, const uint8_t* payload, uint8_t payloadSize);
 
-    int32_t ValidatePacketHeader(PacketHeader* header);
-    bool ValidatePacket(Packet* packet);
+    int32_t ValidatePacketHeader(const PacketHeader& header);
+    bool ValidatePacket(Packet& packet);
 
 }
