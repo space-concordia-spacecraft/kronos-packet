9ba5ad5f62e47f65f617613aaed1af30948fcf0e
Ruben1729 <ruben.e.sanchez.a@gmail.com>
2023-01-26 14:42:50 -0500

    New commands

--- src/ks_command_ids.h
+++ src/ks_command_ids.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #define KS_CMD_PING                 0
-#define KS_CMD_ECHO                 1
-#define KS_CMD_DOWNLINK_BEGIN       2
-#define KS_CMD_DOWNLINK_PART        3 // From satellite, sending part of a file/buffer
-#define KS_CMD_DOWNLINK_FETCH       4 // From groundstation, request specific parts of a file/buffer
-#define KS_CMD_DOWNLINK_CONTINUE    5 // From groundstation, after you've received successfully x packets
+#define KS_CMD_TLE                  1
+#define KS_CMD_ECHO                 2
+#define KS_CMD_DOWNLINK_BEGIN       3
+#define KS_CMD_DOWNLINK_PART        4 // From satellite, sending part of a file/buffer
+#define KS_CMD_DOWNLINK_FETCH       5 // From groundstation, request specific parts of a file/buffer
+#define KS_CMD_DOWNLINK_CONTINUE    6 // From groundstation, after you've received successfully x packets
+#define KS_CMD_LIST_FILES           7
