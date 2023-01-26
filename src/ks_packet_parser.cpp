a43821d54bd62666380d91c5017d4f942a94b39d
Ruben1729 <ruben.e.sanchez.a@gmail.com>
2023-01-26 17:41:58 -0500

    Encode Packet Part

--- src/ks_packet_parser.cpp
+++ src/ks_packet_parser.cpp
@@ -43,6 +43,30 @@ namespace kronos {
         dest.Header.CheckSum = CRC32((uint8_t*)&dest, sizeof(dest.Header) + dest.Header.PayloadSize);
     }
 
+    void EncodePacketPart(
+        Packet& dest,
+        uint8_t flags,
+        uint16_t command,
+        uint16_t part,
+        const uint8_t* payload,
+        uint8_t payloadSize
+    ) {
+        dest = {
+            .Header = {
+                .Magic = KSP_MAGIC,
+                .PacketId = s_NextPacketId++,
+                .PacketFlags = flags,
+                .CommandId = command,
+                .CheckSum = 0,
+                .PayloadSize = (uint8_t)(payloadSize + sizeof(part))
+            },
+            .Payload = {}
+        };
+        memcpy(dest.Payload, &part, sizeof(part));
+        memcpy(dest.Payload + sizeof(part), payload, payloadSize);
+        dest.Header.CheckSum = CRC32((uint8_t*)&dest, sizeof(dest.Header) + dest.Header.PayloadSize);
+    }
+
     int32_t ValidatePacketHeader(const PacketHeader& header) {
         if (header.Magic != KSP_MAGIC)
             return -1;
